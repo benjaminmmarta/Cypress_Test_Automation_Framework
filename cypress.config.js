3885e580d2985952caeb9725e61b586432b29bc7
Benjamin Marta <306624792+benjaminmmarta@users.noreply.github.com>
2026-08-23 21:49:39 -0700

    Updated Files

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "https://parabank.parasoft.com/parabank",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('registration', () => {
+  it('Click Register Link and Complete Registration Form.', () => {
+    cy.visit('/');
+    cy.get('a[href="register.htm"]').click();
+    cy.fixture('registration').then((data) => {
+    data.userName = Date.now();
+    cy.get('#customer\\.firstName').type(data.firstName);
+    cy.get('#customer\\.lastName').type(data.lastName);
+    cy.get('#customer\\.address\\.street').type(data.address);
+    cy.get('#customer\\.address\\.city').type(data.city);
+    cy.get('#customer\\.address\\.state').type(data.state);
+    cy.get('#customer\\.address\\.zipCode').type(data.zipCode);
+    cy.get('#customer\\.phoneNumber').type(data.phoneNumber);
+    cy.get('#customer\\.ssn').type(data.ssn);
+    cy.get('#customer\\.username').type(data.userName);
+    cy.get('#customer\\.password').type(data.password);
+    cy.get('#repeatedPassword').type(data.password);
+    cy.get('[class="button"][value="Register"]').click();
+    cy.get('[class="title"]').should('contain','Welcome ' + data.userName);
+    cy.get('#rightPanel p').should('contain', 'Your account was created successfully. You are now logged in.');
+    })
   })
-})
+});
